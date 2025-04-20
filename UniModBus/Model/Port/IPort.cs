84b860253874b6b06e9c99a0a0c7612820f30d25
murphyqwek <qwekinaft@list.ru>
2025-04-20 23:57:31 +0300

    Create IPort interface

---
+++ UniModBus/Model/Port/IPort.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniModBus.Model.Port
+{
+    public interface IPort
+    {
+        public bool IsOpen();
+        public void Close();
+        public void Open();
+
+        public byte[] ReadData();
+
+        public void WriteData(byte[] data);
+        public void WriteData(string data);
+    }
+}
